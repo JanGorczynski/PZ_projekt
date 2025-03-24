4780a78fb191a23fef63ec0c6817add0c334f16c
jk-0022 <jan.kapusta111@gmail.com>
2025-03-24 03:04:18 +0100

    added submarines and additional tests

--- submarine.py
+++ submarine.py
@@ -1,12 +1,6 @@
 import numpy as np
 import random
 
-def place_shipwreck(Z):
-    x_wreck = random.randint(0, len(Z) - 1)
-    y_wreck = random.randint(0, len(Z) - 1)
-    z_wreck = Z[x_wreck, y_wreck]
-    return x_wreck, y_wreck, z_wreck
-
 def detection_probability(distance, detection_range=100.0):
     if distance <= detection_range:
         return 1.0
@@ -25,13 +19,6 @@ class Submarine:
     def get_position(self):
         return (self.x, self.y, self.z)
 
-    def generate_report(self, wreck_pos):
-        bx, by, bz = self.get_position()
-        wx, wy, wz = wreck_pos
-        dist = np.sqrt((bx - wx)**2 + (by - wy)**2 + (bz - wz)**2)
-        prob = detection_probability(dist, detection_range=100.0)
-        return (bx, by, bz), prob
-
     def pick_new_waypoint(self, Z):
         size = len(Z) - 1
         self.target_x = random.uniform(0, size)
@@ -45,31 +32,35 @@ class Submarine:
         dist_2d = np.sqrt(dx**2 + dy**2)
         if dist_2d < threshold:
             self.pick_new_waypoint(Z)
-        self.move_toward(self.target_x, self.target_y, self.z)
+        self.move_toward(self.target_x, self.target_y)
 
-    def move_toward(self, tx, ty, tz):
+    def move_toward(self, tx, ty):
         dx = tx - self.x
         dy = ty - self.y
-        dz = tz - self.z 
-        dist = np.sqrt(dx**2 + dy**2 + dz**2)
+        dist = np.sqrt(dx**2 + dy**2)
         if dist > 0:
             step = min(self.speed, dist)
             frac = step / dist
             self.x += dx * frac
             self.y += dy * frac
 
-def simulate_search(Z, wreck_pos, submarines, time_steps=200,
-                    detect_threshold=0.2):
-    x_w, y_w, z_w = wreck_pos
+    def generate_report(self, wrecks):
+        bx, by, bz = self.get_position()
+        min_dist = float('inf')
+        for wreck in wrecks:
+            wx, wy, wz = wreck
+            d = np.sqrt((bx - wx)**2 + (by - wy)**2 + (bz - wz)**2)
+            if d < min_dist:
+                min_dist = d
+        prob = detection_probability(min_dist, detection_range=100.0)
+        return (bx, by, bz), prob
+def simulate_random(Z, submarines, wrecks, time_steps=200):
     history = []
     for _ in range(time_steps):
         step_data = []
         for sub in submarines:
-            pos, prob = sub.generate_report((x_w, y_w, z_w))
-            if prob > detect_threshold:
-                sub.move_toward(x_w, y_w, sub.z)
-            else:
-                sub.navigate_random_waypoint(Z)
-            step_data.append({'pos': sub.get_position(), 'prob': prob})
+            sub.navigate_random_waypoint(Z)
+            pos, prob = sub.generate_report(wrecks)
+            step_data.append({'pos': pos, 'prob': prob})
         history.append(step_data)
     return history
